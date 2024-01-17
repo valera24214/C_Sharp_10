efb5bebc99774c798710eabf2eddc837c6e4c16c
Валерий Астроух <valera.astroukh@mail.ru>
2024-01-17 17:28:00 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿
+namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4});
+            this.dataGridView1.Location = new System.Drawing.Point(22, 25);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(579, 202);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(187, 238);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(248, 49);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Создать таблицу";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Регион";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Лыжи";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Санки";
+            this.Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Коньки";
+            this.Column4.Name = "Column4";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(613, 292);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.Office.Interop.Excel;
+using System;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            int A = 0, B = 0, C = 0;
+            Excel.Application excel = new Excel.Application();
+            Object t = Type.Missing;
+            excel.Visible = true;
+            excel.SheetsInNewWorkbook = 3;
+            excel.Workbooks.Add(Type.Missing);
+            var book = excel.Workbooks[1];
+            var sheets = excel.Worksheets;
+            var worksheet = (Excel.Worksheet)sheets.get_Item(1);
+            var excelcell = worksheet.Cells;
+            excelcell = worksheet.get_Range("A1", "E7");
+            excelcell.Borders.ColorIndex = 1;
+            excelcell.Borders.Weight = 3;
+            worksheet.get_Range("A1", t).Value2 = "Регион";
+            worksheet.get_Range("B1", t).Value2 = "Лыжи";
+            worksheet.get_Range("C1", t).Value2 = "Коньки";
+            worksheet.get_Range("D1", t).Value2 = "Санки";
+            worksheet.get_Range("E1", t).Value2 = "Всего";
+
+            int rowNumber = 2;
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                if (row.Index == dataGridView1.Rows.Count - 1)
+                {
+                    continue;
+                }
+
+                worksheet.get_Range("A" + rowNumber, t).Value2 = row.Cells[0].Value;
+                worksheet.get_Range("B" + rowNumber, t).Value2 = row.Cells[1].Value;
+                worksheet.get_Range("C" + rowNumber, t).Value2 = row.Cells[2].Value;
+                worksheet.get_Range("D" + rowNumber, t).Value2 = row.Cells[3].Value;
+
+                var a = Int32.Parse(row.Cells[1].Value.ToString());
+                var b = Int32.Parse(row.Cells[2].Value.ToString());
+                var c = Int32.Parse(row.Cells[3].Value.ToString());
+
+                A += a;
+                B += b;
+                C += c;
+
+                worksheet.get_Range("E" + rowNumber, t).Value2 = a + b + c;
+                rowNumber++;
+                if (rowNumber == 8)
+                {
+                    break;
+                }
+            }
+
+            worksheet.get_Range("A" + rowNumber, t).Value2 = "Среднее";
+            worksheet.get_Range("B" + rowNumber, t).Value2 = Math.Round((decimal)A / (dataGridView1.Rows.Count - 1));
+            worksheet.get_Range("C" + rowNumber, t).Value2 = Math.Round((decimal)B / (dataGridView1.Rows.Count - 1));
+            worksheet.get_Range("D" + rowNumber, t).Value2 = Math.Round((decimal)C / (dataGridView1.Rows.Count - 1));
+
+            Chart graph = (Excel.Chart)excel.Charts.Add(t, t, t, t);
+            // graph.SetSourceData(worksheet.get_Range("E2", "E6"), XlRowCol.xlColumns);
+            // graph.ChartType = XlChartType.xlColumnClustered;
+            graph.HasLegend = false;
+            graph.HasTitle = true;
+            graph.ChartTitle.Caption = "Ведомость";
+            Axis hAxis = (Excel.Axis)graph.Axes(XlAxisType.xlCategory, XlAxisGroup.xlPrimary);
+            hAxis.HasTitle = false;
+            Axis vAxis = (Excel.Axis)graph.Axes(XlAxisType.xlValue, XlAxisGroup.xlPrimary);
+            vAxis.HasTitle = false;
+
+            Object FileName = @"D:\Учет.xls";
+            Object FileFormat = Type.Missing;
+
+            Object Password = Type.Missing;
+
+            Object WriteRes = Type.Missing;
+            Object ReadOnlyRecommended = Type.Missing;
+            Object CreateBac = Type.Missing;
+            Object Confkict = Type.Missing;
+            Object AddTo = Type.Missing;
+            Object TextCode = Type.Missing;
+            Object TextVisual = Type.Missing;
+            Object Local = Type.Missing;
+
+            excel.ActiveWorkbook.SaveAs(
+                FileName, FileFormat,
+                Password, WriteRes, ReadOnlyRecommended,
+                CreateBac, XlSaveAsAccessMode.xlNoChange, Confkict, AddTo, TextCode,
+                TextVisual, Local);
+
+
+            excel.Quit();
+
+        }
+    }
+
+}
